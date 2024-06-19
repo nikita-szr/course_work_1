b8a5b9e7d7d496f6fa4744bf12399e9f087bf6c7
Nikita Osipov <Nikittaosipov@yandex.ru>
2024-06-19 20:09:29 +0400

    get cards data func added

--- src/views.py
+++ src/views.py
@@ -41,3 +41,34 @@ def greeting():
     else:
         return "Доброй ночи"
 
+
+def get_cards_data(transactions):
+    """Функция создает словарь с ключоми номеров карт и в значения добавляет сумму трат и сумму кэшбека"""
+    card_data = {}
+    for transaction in transactions:
+        card_number = transaction.get('Номер карты')
+        # если поле номер карты пустое операцию пропускаем т.к. не понятно к какой карте привязать трату
+        if not card_number or str(card_number).strip().lower() == 'nan':
+            continue
+        amount = float(transaction['Сумма операции'])
+        if card_number not in card_data:
+            card_data[card_number] = {'total_spent': 0.0, 'cashback': 0.0}
+        if amount < 0:
+            card_data[card_number]['total_spent'] += amount
+            cashback_value = transaction.get("Кэшбэк")
+            # рассчитываем кэшбек как 1% от траты, но если поле кешбек содержит сумму просто ее добавляем
+            if cashback_value is not None:
+                cashback_amount = float(cashback_value)
+                if cashback_amount >= 0:
+                    card_data[card_number]['cashback'] += cashback_amount
+                else:
+                    card_data[card_number]['cashback'] += amount * -0.01
+            else:
+                card_data[card_number]['cashback'] += amount * 0.01
+    cards_data = []
+    for last_digits, data in card_data.items():
+        cards_data.append({
+            "last_digits": last_digits,
+            "total_spent": round(data['total_spent'], 2),
+            "cashback": round(data['cashback'], 2)})
+    return cards_data
